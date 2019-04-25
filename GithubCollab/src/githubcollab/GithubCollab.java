7c26212c1fa35c8ce6a1468327c26e5e4eb05102
Wh1te0ne <43663184+Wh1te0ne@users.noreply.github.com>
2019-04-25 04:28:07 -0400

    Update GithubCollab.java

--- GithubCollab/src/githubcollab/GithubCollab.java
+++ GithubCollab/src/githubcollab/GithubCollab.java
@@ -1,21 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package githubcollab;
 
-/**
- *
- * @author brandon
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GithubCollab {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static ArrayList<String> resultList = new ArrayList<>();
+    public static ArrayList<String> files = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        getAllFiles();
+        readAllFiles();
+        resultList.forEach((n) -> System.out.println(n));
     }
-    
+
+    private static void getAllFiles() throws IOException {
+        String userDir = System.getProperty("user.dir");
+        File file = new File(userDir + "/.." + "\\CollabWithGithub");
+        File[] tempList = file.listFiles();
+
+        for (int i = 0; i < tempList.length; i++) {
+            if (tempList[i].isFile()) {
+                files.add(tempList[i].toString());
+            }
+            if (tempList[i].isDirectory()) {
+            }
+        }
+    }
+
+    private static void readAllFiles() {
+        files.forEach((f) -> {
+            try {
+                readFile(f);
+            } catch (IOException ex) {
+                Logger.getLogger(GithubCollab.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+    }
+
+    private static void readFile(String fileName) throws FileNotFoundException, IOException {
+        File inputFile = new File(fileName);
+        try (Scanner in = new Scanner(inputFile)) {
+            String result = "";
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                if (line.contains("Username")) {
+                    result += line + " ";
+                }
+
+                if (line.contains("Password")) {
+                    result += line;
+                }
+            }
+            if (result.length() != 0) {
+                resultList.add(result);
+            }
+        }
+    }
+
 }
